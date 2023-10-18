51b1c55489796f27033df83aecaed3afc6f67884
bradleydlawson <bradleydlawson@outlook.com>
2023-10-18 16:42:11 -0500

    fixed error

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "pipelineagent" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "bdl-rggo"
+    resource_group_name   = "bdl-rg"
     storage_account_name  = "bdlrgstorage"
     container_name        = "bdlrgstoragecontainer"
     key = "my-infrastructure.tfstate"
