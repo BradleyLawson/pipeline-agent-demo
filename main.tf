bac30b70c505ed56f5b36aebe6a4b9df26020b31
bradleydlawson <bradleydlawson@outlook.com>
2023-10-18 15:18:41 -0500

    added storage

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "pipelineagent" {
   name     = "bdl-rg"
   location = var.location
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = azurerm_resource_group.name
+    storage_account_name  = "bdlrgstorage"
+    container_name        = "bdlrgstoragecontainer"
+    key = "my-infrastructure.tfstate"
+  }
+}
