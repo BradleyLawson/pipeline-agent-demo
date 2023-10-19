7e2268f65305464e5783ee332c64911c39afad50
bradleydlawson <bradleydlawson@outlook.com>
2023-10-18 19:58:23 -0500

    changed to environmental vars

--- providers.tf
+++ providers.tf
@@ -7,7 +7,14 @@ terraform {
   }
 }
 
+variable ARM_CLIENT_ID{}
+variable ARM_CLIENT_SECRET{}
+variable ARM_TENANT_ID{}
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
 }
